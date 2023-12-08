b620b9fc87a7495deca620fd9654096121af4860
likai <over@192.168.0.10>
2023-12-08 15:48:25 +0800

    feat: not calculate gas tip

--- core/state_transition.go
+++ core/state_transition.go
@@ -168,7 +168,7 @@ func TransactionToMessage(tx *types.Transaction, s types.Signer, baseFee *big.In
 	}
 	// If baseFee provided, set gasPrice to effectiveGasPrice.
 	if baseFee != nil {
-		msg.GasPrice = cmath.BigMin(msg.GasPrice.Add(msg.GasTipCap, baseFee), msg.GasFeeCap)
+		msg.GasPrice = cmath.BigMin(msg.GasPrice.Add(big.NewInt(0), baseFee), msg.GasFeeCap)
 	}
 	var err error
 	msg.From, err = types.Sender(s, tx)
